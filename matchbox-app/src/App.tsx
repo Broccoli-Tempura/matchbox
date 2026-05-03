8bb78054d624d746a112774e62cc0391e665fe9a
Broccoli Tempura <roswitha.gobbo@student.unisg.ch>
2026-05-04 01:16:54 +0200

    debugging

--- matchbox-app/src/App.tsx
+++ matchbox-app/src/App.tsx
@@ -1,16 +1,16 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { HomeScreen } from "./HomeScreen";
 import { SelectExtinguisher } from "./SelectExtinguisher";
-import { MatchDetail } from "./MatchDetail";   // optional for the others
+import { MatchDetail } from "./MatchDetail";
 
 export default function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Route path="/" element={<HomeScreen />} />
-      <Route path="/select-extinguisher" element={<SelectExtinguisher />} />
-      
-      {/* Fallback for other matches */}
-      <Route path="/match/:id" element={<MatchDetail />} />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/select-extinguisher" element={<SelectExtinguisher />} />
+        <Route path="/match/:id" element={<MatchDetail />} />
+      </Routes>
     </BrowserRouter>
   );
 }
